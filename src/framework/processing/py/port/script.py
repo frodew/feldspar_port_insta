b369ca5dd120cbafcaa5520a0896d08e67d9965f
schlueter-m <maria.schlueter-mp@web.de>
2024-02-03 18:53:28 +0100

    add extraction functions

--- src/framework/processing/py/port/script.py
+++ src/framework/processing/py/port/script.py
@@ -3,6 +3,7 @@ from port.api.assets import *
 from port.api.commands import (CommandSystemDonate, CommandSystemExit, CommandUIRender)
 
 from datetime import datetime, timezone, timedelta
+
 import zipfile
 #from ddpinspect import instagram
 
@@ -111,7 +112,8 @@ def doSomethingWithTheFile(filename):
         try:
             target_df = v["extraction_function"](target_file)
         
-        except:
+        except Exception as e:
+            print(e)
             target_df = pd.DataFrame(["Empty"], columns=[str(file)])
         
         data.append(target_df)
