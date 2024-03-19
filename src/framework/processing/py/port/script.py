2f2536a2df388a536481091cbe6f8546a2977edc
gangolf <friederrodewald@web.de>
2024-03-19 17:59:37 +0100

    add face detection extraction functions and start rebasing and commenting code

--- src/framework/processing/py/port/script.py
+++ src/framework/processing/py/port/script.py
@@ -3,11 +3,11 @@ from port.api.assets import *
 from port.api.commands import (CommandSystemDonate, CommandSystemExit, CommandUIRender)
 
 from datetime import datetime, timezone, timedelta
-
 import zipfile
 import cv2
+from PIL import Image
 import numpy as np
-#from ddpinspect import instagram
+import time
 
 import pandas as pd
 import json
@@ -15,9 +15,7 @@ import json
 from port.extraction_functions import *
 from port.extraction_functions_dict import extraction_dict
 
-
-
-# all functions during the donation process are called here
+# MAIN FUNCTION INITIATING THE DONATION PROCESS
 
 def process(sessionId):
     print(read_asset("hello_world.txt"))
@@ -26,22 +24,36 @@ def process(sessionId):
     meta_data = []
     meta_data.append(("debug", f"{key}: start"))
 
-    # STEP 1: select the file
+    # STEP 1: Select DDP and extract automatically required data
+    
     data = None
+
     while True:
         meta_data.append(("debug", f"{key}: prompt file"))
-        promptFile = prompt_file("application/zip, text/plain")
+
+        # allow users to only upload zip-files and render file-input page 
+        promptFile = prompt_file("application/zip")
         fileResult = yield render_donation_page(promptFile)
+        
+        # if user input
         if fileResult.__type__ == 'PayloadString':
+
             meta_data.append(("debug", f"{key}: extracting file"))
-            extractionResult = doSomethingWithTheFile(fileResult.value)
+
+            # automatically extract required data
+            extractionResult = extract_data(fileResult.value)
+
             if extractionResult != 'invalid':
+
                 meta_data.append(("debug", f"{key}: extraction successful, go to consent form"))
                 data = extractionResult
                 break
+
             else:
+
                 meta_data.append(("debug", f"{key}: prompt confirmation to retry file selection"))
                 retry_result = yield render_donation_page(retry_confirmation())
+
                 if retry_result.__type__ == 'PayloadTrue':
                     meta_data.append(("debug", f"{key}: skip due to invalid file"))
                     continue
@@ -49,7 +61,8 @@ def process(sessionId):
                     meta_data.append(("debug", f"{key}: retry prompt file"))
                     break
 
-    # STEP 2: ask for consent
+    # STEP 2: Present user their extracted data and ask for consent
+
     meta_data.append(("debug", f"{key}: prompt consent"))
     prompt = prompt_consent(data, meta_data)
     consent_result = yield render_donation_page(prompt)
@@ -64,85 +77,105 @@ def process(sessionId):
 
 # render pages used in process
 
-def render_donation_page(body):
-    header = props.PropsUIHeader(props.Translatable({
-        "en": "Port flow example",
-        "nl": "Port voorbeeld flow"
-    }))
-
-    page = props.PropsUIPageDonation("Zip", header, body)
-    return CommandUIRender(page)
-
-
-def retry_confirmation():
-    text = props.Translatable({
-        "en": "Unfortunately, we cannot process your file. Continue, if you are sure that you selected the right file. Try again to select a different file.",
-        "nl": "Helaas, kunnen we uw bestand niet verwerken. Weet u zeker dat u het juiste bestand heeft gekozen? Ga dan verder. Probeer opnieuw als u een ander bestand wilt kiezen."
-    })
-    ok = props.Translatable({
-        "en": "Try again",
-        "nl": "Probeer opnieuw"
-    })
-    cancel = props.Translatable({
-        "en": "Continue",
-        "nl": "Verder"
-    })
-    return props.PropsUIPromptConfirm(text, ok, cancel)
-
-
 def prompt_file(extensions):
     description = props.Translatable({
-        "en": "Please select any zip file stored on your device.",
+        "en": "Please select your downloaded Instagram ZIP file from your device. The file should be named like \"instagram-USERNAME-DATE-...-.zip\".",
         "nl": "Selecteer een willekeurige zip file die u heeft opgeslagen op uw apparaat."
     })
 
     return props.PropsUIPromptFileInput(description, extensions)
 
+def render_donation_page(body):
+
+    platform = "Instagram"
+
+    header = props.PropsUIHeader(props.Translatable({
+        "en": "Instagram Data Donation",
+        "nl": "Port voorbeeld flow"
+    }))
+    
+    page = props.PropsUIPageDonation(platform, header, body)
+
+    return CommandUIRender(page)
+
+
+def retry_confirmation():
+
+    text = props.Translatable({
+        "en": "Unfortunately, we cannot process your file. Are you sure that you selected the downloaded Instagram ZIP file? The file should be named like \"instagram-USERNAME-DATE-...-.zip\". Make sure it is a zip file.",
+        "nl": "Helaas, kunnen we uw bestand niet verwerken. Weet u zeker dat u het juiste bestand heeft gekozen? Ga dan verder. Probeer opnieuw als u een ander bestand wilt kiezen."
+    })
+
+    ok = props.Translatable({
+        "en": "Try again",
+        "nl": "Probeer opnieuw"
+    })
+
+    cancel = props.Translatable({
+        "en": "Continue",
+        "nl": "Verder"
+    })
+
+    return props.PropsUIPromptConfirm(text, ok, cancel)
+
+
+
 
 # extraction of zip files -------------------------------------------------
 
 #main function to process zip files
-def doSomethingWithTheFile(filename): 
+def extract_data(filename): 
     """takes zip folder, extracts relevant json file contents (your_topics, posts_viewed, videos_watched), then extracts & processes relevant information and returns them as dataframes"""
     
-
-
     def check_faces_in_zip(filename):
+        """This function checks the number of faces in each image file within a zip file."""
 
         face_dict = {}
 
-        with zipfile.ZipFile(filename, 'r') as zip_ref:
-            for file in zip_ref.namelist():
-                if file.lower().endswith('.jpg'):
-                    
-                    with zip_ref.open(file) as img_file:
-                        print(img_file)
-                        img_data = img_file.read()
-                        nparr = np.frombuffer(img_data, np.uint8)
-                        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        # Load face cascade classifier
+        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-                        # Skip small images
-                        if min(image.shape[:2]) < 30:
-                            face_dict[file] = False
-                            continue
+        # Set the desired size for the images
+        size = 200, 200
+
+        # Open the zip file in read mode
+        with zipfile.ZipFile(filename, 'r') as zip_ref:
+
+            # Iterate through each file in the zip file
+            for file in zip_ref.namelist():
+
+                # Check if the file is a jpg image and in the media folder
+                if file.lower().endswith('.jpg') and file.lower().startswith('media'):
+
+                    # Open the image file within the zip file
+                    with zip_ref.open(file) as img_file:
+
+                        start_time = time.time()  # Record start time
+
+                        # Load the image from bytes
+                        img_bytes = Image.open(img_file)
 
                         # Convert the image to grayscale
-                        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                        img_gray = img_bytes.convert("L")
 
-                        # Load the pre-trained face cascade
-                        face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+                        # Resize the image to the desired size
+                        img_gray.thumbnail(size, Image.Resampling.LANCZOS)
+
+                        # Convert the image to a numpy array
+                        img_down_np = np.array(img_gray)
 
                         # Detect faces in the image
-                        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+                        faces = face_cascade.detectMultiScale(img_down_np, scaleFactor=1.1, minNeighbors=6, minSize=(30, 30))
+                        
+                        end_time = time.time()  # Record end time
+                        processing_time = end_time - start_time  # Calculate processing time
+                        print("PLI - Processing time for {}: {:.2f} seconds".format(file, processing_time))
 
-                        # Check if faces were detected
-                        if len(faces) > 0:
-                            face_dict[file] = True
-                        else:
-                            face_dict[file] = False
+                        # Count faces in picture
+                        face_dict[file] = len(faces)
 
-
-        return face_dict    
+        # Return the dictionary containing the number of faces in each image
+        return face_dict
 
     picture_info = check_faces_in_zip(filename)
 
@@ -159,8 +192,8 @@ def doSomethingWithTheFile(filename):
                 target_df = v["extraction_function"](target_file)
 
         except Exception as e:
-            print(e)
-            target_df = pd.DataFrame(["Empty"], columns=[str(file)])
+            print(target_file, e)
+            target_df = pd.DataFrame(["file_does_not_exist"], columns=[str(file)])
         
         data.append(target_df)
 
